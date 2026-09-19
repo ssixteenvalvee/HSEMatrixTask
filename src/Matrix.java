fbc6e7949335b85977a39dd45e1179dfd08483e6
ssixteenvalvee <nb-775@yandex.ru>
2026-09-19 17:54:40 +0300

    Common Matrix operations.

--- src/Matrix.java
+++ src/Matrix.java
@@ -22,7 +22,9 @@ public class Matrix {
     public int getRows() {return Rows;}
     public int getCols() {return Cols;}
     public Complex[][] getA() {return A;}
-
+    public Complex get(int r, int c) {return A[r][c];}
+    //set
+    public void set(Complex val, int r, int c) {A[r][c] = val;}
     // methods
     public void fill() {
         Scanner scanner = new Scanner(System.in);
@@ -83,5 +85,77 @@ public class Matrix {
             det = det.prod(A[i][i]);
         }
         return det;
+    }
+
+    public Matrix plusw(Matrix B) {
+        if (this.Rows != B.Rows || this.Cols != B.Cols) {
+            throw new IllegalArgumentException("Only with single-dimension matrix.");
+        }
+        Matrix C = new Matrix(this.Rows, this.Cols);
+        for (int i = 0; i < this.Rows; i++) {
+            for (int j = 0; j < this.Cols; j++) {
+                Complex c_elem = A[i][j].plus(B.getA()[i][j]);
+                C.getA()[i][j] = c_elem;
+            }
+        }
+        return C;
+    }
+
+    public Matrix minusw(Matrix B) {
+        if (this.Rows != B.Rows || this.Cols != B.Cols) {
+            throw new IllegalArgumentException("Only with single-dimension matrix.");
+        }
+        Matrix C = new Matrix(this.Rows, this.Cols);
+        for (int i = 0; i < this.Rows; i++) {
+            for (int j = 0; j < this.Cols; j++) {
+                Complex c_elem = A[i][j].minus(B.getA()[i][j]);
+                C.getA()[i][j] = c_elem;
+            }
+        }
+        return C;
+    }
+
+    public Matrix prodw(Matrix B) {
+        if (this.Cols != B.Rows) {
+            throw new IllegalArgumentException( //
+                    "Cannot multiply: A is " + this.Rows + "x" + this.Cols +
+                    ", B is " + B.Rows + "x" + B.Cols +
+                    ". Need A.Cols == B.Rows.");
+        }
+        Matrix C = new Matrix(this.Rows, B.getCols());
+        Complex[][] a = this.A; // massive - matrix
+        Complex[][] b = B.getA(); // massive - matrix
+        for (int i = 0; i < this.Rows; i++) {
+            for (int j = 0; j < B.getCols(); j++) {
+                Complex cij = new Complex(0, 0);
+                for (int k = 0; k < this.Cols; k++) {
+                    cij = cij.plus(a[i][k].prod(b[k][j]));
+                }
+                C.set(cij, i, j); // value, row, col;
+            }
+        }
+        return C;
+    }
+
+    public Matrix divw(Matrix B) {
+        if (this.Cols != B.Rows) {
+            throw new IllegalArgumentException( //
+                    "Cannot divide: A is " + this.Rows + "x" + this.Cols +
+                            ", B is " + B.Rows + "x" + B.Cols +
+                            ". Need A.Cols == B.Rows.");
+        }
+        Matrix C = new Matrix(this.Rows, B.getCols());
+        Complex[][] a = this.A; // massive - matrix
+        Complex[][] b = B.getA(); // massive - matrix
+        for (int i = 0; i < this.Rows; i++) {
+            for (int j = 0; j < B.getCols(); j++) {
+                Complex cij = new Complex(0, 0);
+                for (int k = 0; k < this.Cols; k++) {
+                    cij = cij.plus(a[i][k].div(b[k][j]));
+                }
+                C.set(cij, i, j); // value, row, col;
+            }
+        }
+        return C;
     }
 }
